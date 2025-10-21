9d45db555188720c9914fec71d8c2b629a51f4d9
kitotake <76629813+kitotake@users.noreply.github.com>
2025-10-21 02:31:38 +0200

    update

--- client/nui_handler.lua
+++ client/nui_handler.lua
@@ -1,19 +1,63 @@
+RegisterCommand('openmenu', function()
+    SetNuiFocus(true, true)
+    
+    SendNUIMessage({
+        action = 'openMenu',
+        data = {
+            characters = {
+                {
+                    id = 1,
+                    firstname = 'John',
+                    lastname = 'Doe',
+                    dateofbirth = '1990-05-15',
+                    gender = 'male',
+                    height = 180
+                },
+                {
+                    id = 2,
+                    firstname = 'Jane',
+                    lastname = 'Smith',
+                    dateofbirth = '1995-08-22',
+                    gender = 'female',
+                    height = 165
+                }
+            },
+            maxCharacters = 5
+        }
+    })
+end)
+
 RegisterNUICallback('selectCharacter', function(data, cb)
-    TriggerServerEvent('kt_identity:selectCharacter', data.charId)
+    print('Personnage sélectionné:', data.characterId)
+    SetNuiFocus(false, false)
+    
+    TriggerServerEvent('kt_identity:selectCharacter', data.characterId)
+    
     cb('ok')
 end)
 
 RegisterNUICallback('createCharacter', function(data, cb)
+    print('Création personnage:', json.encode(data))
+    
     TriggerServerEvent('kt_identity:createCharacter', data)
+    
     cb('ok')
 end)
 
 RegisterNUICallback('deleteCharacter', function(data, cb)
-    TriggerServerEvent('kt_identity:deleteCharacter', data.charId)
+    print('Suppression personnage:', data.characterId)
+    
+    TriggerServerEvent('kt_identity:deleteCharacter', data.characterId)
+    
     cb('ok')
 end)
 
-RegisterCommand('reloadchar', function()
-    SetNuiFocus(true, true)
-    SendNUIMessage({ action = 'openCharacterMenu' })
+RegisterNUICallback('closeMenu', function(data, cb)
+    SetNuiFocus(false, false)
+    cb('ok')
+end)
+
+RegisterNUICallback('escape', function(data, cb)
+    SetNuiFocus(false, false)
+    cb('ok')
 end)
